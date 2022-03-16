3d9e2c550df237e6c640cfd065fe6b03b62d7aeb
Alaa Tujjar <alaat>
2022-03-16 16:23:16 -0600

    Add js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById("buy")
+
+console.log(document.getElementById("buy") )
+
+ file1= document.getElementById("buy")
+
+console.log ( typeof file1 ) 
+
+file1.style.color = "white"
+
+file1.style.backgroundColor="red"
